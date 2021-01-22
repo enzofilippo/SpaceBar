a46ec2e278f3aff21a343859e2f5ccc5adb1a48b
enzofilippo <42002318+enzofilippo@users.noreply.github.com>
2021-01-22 11:30:16 -0300

    Removendo comentários
    Removi comentários desnecessários, utilizados para aprendizagem.

--- SpaceBar Game/main.c
+++ SpaceBar Game/main.c
@@ -15,42 +15,28 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Inclui o cabeçalho da bibilioteca Allegro 5
-#include <allegro5/allegro.h>
 
-// Inclui o cabeçalho do add-on para uso de imagens
+#include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
 int main(void)
 {
-    // Variável representando a janela principal
-    ALLEGRO_DISPLAY *janela = NULL;
 
-    // Variável representando a imagem
+    ALLEGRO_DISPLAY *janela = NULL;
     ALLEGRO_BITMAP *imagem = NULL;
 
-    // Inicializa a Allegro
     al_init();
-
-    // Inicializa o add-on para utilização de imagens
     al_init_image_addon();
 
-    // Configura a janela
     janela = al_create_display(640, 480);
 
-    // Carrega a imagem
     imagem = al_load_bitmap("sprites/SpriteLogo.bmp");
 
-    // Desenha a imagem na tela
     al_draw_bitmap(imagem, 0, 0, 0);
-
-    // Atualiza a tela
     al_flip_display();
 
-    // Segura a execução por 10 segundos
     al_rest(10.0);
 
-    // Finaliza a janela
     al_destroy_display(janela);
 
     return 0;
