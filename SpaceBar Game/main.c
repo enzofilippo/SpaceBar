ae4ff438d404c1ebaf8e5c23f9c8937c63a699b0
enzofilippo <42002318+enzofilippo@users.noreply.github.com>
2021-01-22 11:26:27 -0300

    Adicionando logo
    Implementando a abertura do jogo com a logo.

--- SpaceBar Game/main.c
+++ SpaceBar Game/main.c
@@ -1,20 +1,48 @@
-// Inclui o arquivo de cabeçalho da biblioteca Allegro 5
+/*
+ * This file is part of the enzofilippo distribution (https://github.com/enzofilippo).
+ * Copyright (c) 2021 Enzo Filippo Centenaro da Silva.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Inclui o cabeçalho da bibilioteca Allegro 5
 #include <allegro5/allegro.h>
 
-// Nossa conhecida função main...
+// Inclui o cabeçalho do add-on para uso de imagens
+#include <allegro5/allegro_image.h>
+
 int main(void)
 {
     // Variável representando a janela principal
     ALLEGRO_DISPLAY *janela = NULL;
 
-    // Inicializamos a biblioteca
+    // Variável representando a imagem
+    ALLEGRO_BITMAP *imagem = NULL;
+
+    // Inicializa a Allegro
     al_init();
 
-    // Criamos a nossa janela - dimensões de 640x480 px
+    // Inicializa o add-on para utilização de imagens
+    al_init_image_addon();
+
+    // Configura a janela
     janela = al_create_display(640, 480);
 
-    // Preenchemos a janela de branco
-    al_clear_to_color(al_map_rgb(255, 255, 255));
+    // Carrega a imagem
+    imagem = al_load_bitmap("sprites/SpriteLogo.bmp");
+
+    // Desenha a imagem na tela
+    al_draw_bitmap(imagem, 0, 0, 0);
 
     // Atualiza a tela
     al_flip_display();
