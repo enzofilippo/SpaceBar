3ad205ce4da2168618612f67824cba9b2bc0e104
enzofilippo <42002318+enzofilippo@users.noreply.github.com>
2021-02-16 17:40:26 -0300

    TOmara que de certo

--- SpaceBar Game/main.c
+++ SpaceBar Game/main.c
@@ -26,7 +26,12 @@
 #define LARGURA_TELA 640
 #define ALTURA_TELA 360
 
+<<<<<<< Updated upstream
 double dinheiro = 10;
+=======
+double dinheiro = 0;
+
+>>>>>>> Stashed changes
 double custoBaseDormitorio = 10;
 double custoBaseEstufa = 400;
 double custoBaseQuimica = 6000;
@@ -39,8 +44,16 @@ double prodBaseQuimica = 10;
 double prodBaseFisica = 100;
 double prodBaseRobotica = 1000;
 double prodBaseNuclear = 10000;
+<<<<<<< Updated upstream
 double upgradeMultiplier = 100; //novo jogo - n�vel 0
 double taxaCrescimento = 1.07;
+=======
+double taxaCrescimentoNuclear = 1.10;
+
+double custoUpgradeVodka = 1;
+
+double upgradeMultiplier = 1; //novo jogo - n�vel 0
+>>>>>>> Stashed changes
 
 float red_x;
 float red_y;
@@ -49,6 +62,7 @@ double incrementoPorFrame, dinheiroPorSegundo, custo, prod;
 double custoDormitorio, custoEstufa, custoQuimica, custoFisica, custoRobotica, custoNuclear;
 double qtdeDormitorio, qtdeEstufa, qtdeQuimica, qtdeFisica, qtdeRobotica, qtdeNuclear;
 double prodDormitorio, prodEstufa, prodQuimica, prodFisica, prodRobotica, prodNuclear;
+int upgradeVodka;
 
 ALLEGRO_DISPLAY *janela = NULL;
 ALLEGRO_EVENT_QUEUE *filaEventos = NULL;
@@ -63,7 +77,18 @@ ALLEGRO_BITMAP *botaoLabs2 = NULL;
 ALLEGRO_BITMAP *backgroundDinheiro = NULL;
 ALLEGRO_BITMAP *backgroundUpgrades = NULL;
 ALLEGRO_BITMAP *backgroundDinheiroPorSegundo = NULL;
+<<<<<<< Updated upstream
 ALLEGRO_TIMER *timer = NULL;
+=======
+ALLEGRO_BITMAP *backgroundMonitor = NULL;
+ALLEGRO_BITMAP *backgroundUpgrade = NULL;
+ALLEGRO_BITMAP *backgroundUpgrade2 = NULL;
+ALLEGRO_BITMAP *led = NULL;
+ALLEGRO_TIMER *timer = NULL;
+ALLEGRO_SAMPLE *som_botao = NULL;
+ALLEGRO_SAMPLE *som_semDinheiro = NULL;
+ALLEGRO_AUDIO_STREAM *musica = NULL;
+>>>>>>> Stashed changes
 
 int noBotaoFecharAnterior;
 int noBotaoLabsAnterior;
@@ -81,6 +106,48 @@ int inicializar(){
         return 0;
     }
 
+<<<<<<< Updated upstream
+=======
+    if(!al_install_audio()){
+        error_msg("Falha ao inicializar o audio");
+        return 0;
+    }
+
+    if(!al_init_acodec_addon()){
+        error_msg("Falha ao inicializar o codec de audio");
+        return 0;
+    }
+
+    if (!al_reserve_samples(5)){
+        error_msg("Falha ao reservar amostrar de audio");
+        return 0;
+    }
+
+    som_botao = al_load_sample( "resources/buttonSound.ogg" );
+    if (!som_botao){
+        error_msg( "Audio nao carregado" );
+        return 0;
+    }
+
+    som_semDinheiro = al_load_sample( "resources/semDinheiro.ogg" );
+    if (!som_botao){
+        error_msg( "Audio nao carregado" );
+        al_destroy_sample(som_botao);
+        return 0;
+    }
+
+    musica = al_load_audio_stream("resources/soundtrack.ogg", 4, 1024);
+    if (!musica){
+        al_destroy_sample(som_botao);
+        al_destroy_sample(som_semDinheiro);
+        error_msg( "Audio nao carregado" );
+        return 0;
+    }
+
+    al_attach_audio_stream_to_mixer(musica, al_get_default_mixer());
+    al_set_audio_stream_playmode(musica, ALLEGRO_PLAYMODE_LOOP);
+
+>>>>>>> Stashed changes
     timer = al_create_timer(1.0 / FPS);
     if(!timer) {
         error_msg("Falha ao criar temporizador");
@@ -147,6 +214,13 @@ int inicializar(){
     backgroundDinheiro = al_load_bitmap("resources/SpriteBackgroundDinheiro.png");
     backgroundUpgrades = al_load_bitmap("resources/SpriteBackgroundUpgrades.png");
     backgroundDinheiroPorSegundo = al_load_bitmap("resources/SpriteBackgroundDinheiroPorSegundo.png");
+<<<<<<< Updated upstream
+=======
+    backgroundMonitor = al_load_bitmap("resources/SpriteBackgroundMonitor.png");
+    backgroundUpgrade = al_load_bitmap("resources/SpriteBotaoUpgrade.png");
+    backgroundUpgrade2 = al_load_bitmap("resources/SpriteBotaoUpgrade2.png");
+    led = al_load_bitmap("resources/SpriteLuzLigada.png");
+>>>>>>> Stashed changes
 
     al_draw_bitmap(background, 0, 0, 0);
 
@@ -261,7 +335,17 @@ int main(void){
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 53, 277, ALLEGRO_ALIGN_LEFT, "DORMITORIO: %.0f", qtdeDormitorio);
                         custoDormitorio = custoCalculo(custoBaseDormitorio, qtdeDormitorio);
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 53, 287, ALLEGRO_ALIGN_LEFT, "custo: %.2f", custoDormitorio);
+<<<<<<< Updated upstream
+=======
+                        al_draw_bitmap(backgroundMonitor, 390, 274, 0);
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,273 , ALLEGRO_ALIGN_LEFT, "Eu nem sabia que tinha");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,285 , ALLEGRO_ALIGN_LEFT, "camas no espaço...");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,297 , ALLEGRO_ALIGN_LEFT, "Mas foi você que pagou");
+                    }else{
+                        al_play_sample(som_semDinheiro, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+>>>>>>> Stashed changes
                     }
+
                 }
             }else{
                 if(noBotaoLabsAnterior){
@@ -288,6 +372,9 @@ int main(void){
                         custoEstufa = custoCalculo(custoBaseEstufa, qtdeEstufa);
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 53, 315, ALLEGRO_ALIGN_LEFT, "custo: %.2f", custoEstufa);
                     }
+                    else{
+                        al_play_sample(som_semDinheiro, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+                    }
                 }
             }else{
                 if(noBotaoLabsAnterior){
@@ -313,6 +400,15 @@ int main(void){
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 53, 333, ALLEGRO_ALIGN_LEFT, "QUIMICA: %.0f", qtdeQuimica);
                         custoQuimica = custoCalculo(custoBaseQuimica, qtdeQuimica);
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 53, 343, ALLEGRO_ALIGN_LEFT, "custo: %.2f", custoQuimica);
+<<<<<<< Updated upstream
+=======
+                        al_draw_bitmap(backgroundMonitor, 390, 274, 0);
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,273 , ALLEGRO_ALIGN_LEFT, "Agora sim! Da pra");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,285 , ALLEGRO_ALIGN_LEFT, "fazer muita vodka");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,297 , ALLEGRO_ALIGN_LEFT, "para os cientistas");
+                    }else{
+                        al_play_sample(som_semDinheiro, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+>>>>>>> Stashed changes
                     }
                 }
             }else{
@@ -340,6 +436,9 @@ int main(void){
                         custoFisica = custoCalculo(custoBaseFisica, qtdeFisica);
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 216, 287, ALLEGRO_ALIGN_LEFT, "custo: %.2f", custoFisica);
                     }
+                    else{
+                        al_play_sample(som_semDinheiro, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+                    }
                 }
             }else{
                 if(noBotaoLabsAnterior){
@@ -365,6 +464,15 @@ int main(void){
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 216, 305, ALLEGRO_ALIGN_LEFT, "ROBOTICA: %.0f", qtdeRobotica);
                         custoRobotica = custoCalculo(custoBaseRobotica, qtdeRobotica);
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 216, 315, ALLEGRO_ALIGN_LEFT, "custo: %.2f", custoRobotica);
+<<<<<<< Updated upstream
+=======
+                        al_draw_bitmap(backgroundMonitor, 390, 274, 0);
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,273 , ALLEGRO_ALIGN_LEFT, "Ya! mais robôs!!!");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,285 , ALLEGRO_ALIGN_LEFT, "estranho, pq aquele robô");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,297 , ALLEGRO_ALIGN_LEFT, "está dizendo matar hum...");
+                    }else{
+                        al_play_sample(som_semDinheiro, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+>>>>>>> Stashed changes
                     }
                 }
             }else{
@@ -391,11 +499,63 @@ int main(void){
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 216, 333, ALLEGRO_ALIGN_LEFT, "NUCLEAR: %.0f", qtdeNuclear);
                         custoNuclear = custoCalculo(custoBaseNuclear, qtdeNuclear);
                         al_draw_textf(fonteMenor, al_map_rgb(55, 68, 89), 216, 343, ALLEGRO_ALIGN_LEFT, "custo: %.2f", custoNuclear);
+<<<<<<< Updated upstream
+=======
+                        al_draw_bitmap(backgroundMonitor, 390, 274, 0);
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,273 , ALLEGRO_ALIGN_LEFT, "Kalashnikov, faça");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,285 , ALLEGRO_ALIGN_LEFT, "suas AK-47 atirarem");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,297 , ALLEGRO_ALIGN_LEFT, "projéteis radioativos!!!");
+                    }else{
+                        al_play_sample(som_semDinheiro, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+>>>>>>> Stashed changes
                     }
                 }
             }else{
                 if(noBotaoLabsAnterior){
                     al_draw_bitmap(botaoLabs2, 165, 335, 0);
+                }
+            }
+
+            if (evento.mouse.x >= red_x*570 &&  //botão upgrade Vodka
+                evento.mouse.x <= red_x*637 &&
+                evento.mouse.y <= red_y*75 &&
+                evento.mouse.y >= red_y*50) {
+
+                al_draw_bitmap(backgroundUpgrade, 569, 49, 0);
+                noBotaoLabsAnterior = 1;
+                al_draw_bitmap(backgroundMonitor, 390, 274, 0);
+                al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,273 , ALLEGRO_ALIGN_LEFT, "Compre o upgrade");
+                al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,285 , ALLEGRO_ALIGN_LEFT, "para passar de nível");
+                al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,297 , ALLEGRO_ALIGN_LEFT, "");
+
+                if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
+                    al_play_sample(som_botao, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+
+                    if(!upgradeVodka){
+                        if(custoUpgradeVodka<=dinheiro){
+                        dinheiro -= custoUpgradeVodka;
+                        upgradeMultiplier = 2;
+                        prodDormitorio = prodCalculo(prodBaseDormitorio, qtdeDormitorio, upgradeMultiplier);
+                        prodEstufa = prodCalculo(prodBaseEstufa, qtdeEstufa, upgradeMultiplier);
+                        prodQuimica = prodCalculo(prodBaseQuimica, qtdeQuimica, upgradeMultiplier);
+                        prodFisica = prodCalculo(prodBaseFisica, qtdeFisica, upgradeMultiplier);
+                        prodRobotica = prodCalculo(prodBaseRobotica, qtdeRobotica, upgradeMultiplier);
+                        prodNuclear = prodCalculo(prodBaseNuclear, qtdeNuclear, upgradeMultiplier);
+                        al_draw_bitmap(backgroundMonitor, 390, 274, 0);
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,273 , ALLEGRO_ALIGN_LEFT, "a");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,285 , ALLEGRO_ALIGN_LEFT, "a");
+                        al_draw_textf(fonteMenor, al_map_rgb(85, 255, 0), 393,297 , ALLEGRO_ALIGN_LEFT, "a");
+                        al_draw_bitmap(led, 553, 50, 0);
+                        upgradeVodka = 1;
+                        }else{
+                        al_play_sample(som_semDinheiro, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_ONCE,NULL);
+                        }
+                    }
+                }
+            }else{
+                if(noBotaoLabsAnterior){
+                    al_draw_bitmap(backgroundUpgrade2, 569, 49, 0);
+                    noBotaoLabsAnterior=1;
                 }
             }
 
